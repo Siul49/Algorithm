d32aefcf05a4aeaf0218e9a50e05be0dbadcc7af
Siul <103662328+Siul49@users.noreply.github.com>
2024-11-04 10:53:11 +0900

    [Silver IV] Title: solved.ac, Time: 84 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // 예외 처리: n이 0인 경우
+    if (n == 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    int a = round(n * 0.15);  // 절사할 개수 계산
+    int sum = 0;
+    for (int i = a; i < n - a; i++) {
+        sum += arr[i];
+    }
+
+    // 평균 계산과 반올림
+    double average = static_cast<double>(sum) / (n - 2 * a);
+    cout << round(average) << endl;
+
+    delete[] arr;
+    return 0;
+}
