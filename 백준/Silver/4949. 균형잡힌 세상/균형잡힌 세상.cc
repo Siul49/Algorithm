be93b388b5ce8605ff8984dc86856535f106ece4
Siul <103662328+Siul49@users.noreply.github.com>
2024-11-06 16:48:48 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 92 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -16,8 +16,8 @@ int main() {
 
     while (1) {
         stack<char> s; // 여는 괄호를 넣을 스택 닫는 괄호 나올 때마다 꺼내서 확인한다
-        int cnt1 = 0;
-        int cnt = 0;
+        int cnt1 = 0; // 여는 괄호가 스택에 있는지 없는지
+        int cnt = 0; //
         string arr;
         getline(cin, arr); // 문자열 입력받기
         if (arr == ".") break; // 종료 조건
@@ -26,14 +26,12 @@ int main() {
             if (arr[i] == '.') break;
             if (arr[i] == '(' || arr[i] == '[') {
                 s.push(arr[i]);
-                cnt1++;
             }
             else if (arr[i] == ')' || arr[i] == ']') {
                 cnt++;
-                if (cnt1 > 0) {
+                if (!s.empty()) {
                     if (compare(s.top(), arr[i])) {
                         s.pop();
-                        cnt1--;
                         cnt--;
                     }
                 }
